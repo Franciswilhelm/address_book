978c1beae34b4bde3cacc11f4b14c8aecbc42f09
Francis Prus <francis.prus@hotmail.com>
2016-09-09 17:05:18 -0700

    create contact class with getters and setters.

--- src/Contact.php
+++ src/Contact.php
@@ -5,6 +5,41 @@
         public $number;
         public $address;
 
-        function __construct($)
+        function __construct($friend_name, $phone_number, $friend_address)
+        {
+            $this->name = $friend_name;
+            $this->number = $phone_number;
+            $this->address = $friend_address;
+        }
 
-    }?>
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setNumber($new_number)
+        {
+            $this->number = $new_number;
+        }
+
+        function getNumber()
+        {
+            return $this->number;
+        }
+
+        function setAddress($new_address)
+        {
+            $this->address = $new_address;
+        }
+
+        function getAddress()
+        {
+            return $this->address;
+        }
+      }
+    ?>
